ba80c1ed0d4b8c9b148a86e364ee9d6e0b2c6eae
unknown <tomson.ngassa@TomsonNgassaPC.fei.local>
2014-01-28 10:25:14 -0500

    delete the less and html2js plugins, update the gruntfile tasks and the package.json accordingly

--- src/common/services/patientService.js
+++ src/common/services/patientService.js
@@ -3,7 +3,9 @@
 angular.module('bham.patientService', ['ngResource'])
 
 .factory('PatientService', ['$resource', function($resource){
-	
+	/*
+	http://172.16.100.172:9000/api/patients
+	*/
     var patients = [
 		{  MRN:123456, firstName:'Tomson', lastName:'Ngassa', DOB:'01/11/1990', gender:'Male', race:"African", SSN:123456789},
 		{  MRN:123456, firstName:'Himalay', lastName:'Majumdar', DOB:'01/11/1990', gender:'Male', race:"Asian", SSN:123456789},
@@ -12,22 +14,26 @@ angular.module('bham.patientService', ['ngResource'])
 		{  MRN:123456, firstName:'Joel',lastName:'Amoussou',DOB:'01/11/1990',gender:'Male', race:"African", SSN:123456789}
     ];
 	
+	// return  $resource('http://172.16.100.172:9000/api/patients', 
+				// {}
+			// );
+	
 	return {
-      query: function() { 
-				return patients;
-			},
-      add : function(patient) {	
-                patient.id = patients.length;
-				return patients.push(patient);
-			},
-      get : function(id) { 
-				return patients[id];
-			},
-      update : function(id, patient) {
-				patients[id] = patient;
-			},
-       delete : function(id) {
-			patients.splice(id,1);
-			}		
+			query: function() {
+					return patients;
+				},
+			add : function(patient) {	
+					patient.id = patients.length;
+					return patients.push(patient);
+				},
+			get : function(id) { 
+					return patients[id];
+				},
+			update : function(id, patient) {
+					patients[id] = patient;
+				},
+			delete : function(id) {
+				patients.splice(id,1);
+				}		
     };
 }]);
